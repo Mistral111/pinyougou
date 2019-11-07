01c540fafdab4546b1c38720b8320ee6ee7acbc5
Mistral <237978440@qq.com>
2019-11-07 09:47:18 +0800

    11.07--9:47

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var aa=document.getElementsByClassName("middlefoottopfootright")[0].getElementsByTagName("li")
+for(var i=0;i<14;i++){
+	if(i%2!=0){
+		aa[i].style.borderLeft="1px dashed #e0e0e0"
+	}
+}
